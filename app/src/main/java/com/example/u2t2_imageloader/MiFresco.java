9dbefe2ae608febccfa972f18bb29eb1aa646c1e
Angela Balaguer <angbalaguerv@upt.pe>
2020-02-12 16:25:36 -0500

    fresco

--- app/src/main/java/com/example/u2t2_imageloader/MiFresco.java
+++ app/src/main/java/com/example/u2t2_imageloader/MiFresco.java
@@ -1,22 +1,28 @@
 package com.example.u2t2_imageloader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
 
+import me.relex.photodraweeview.PhotoDraweeView;
+
 public class MiFresco extends AppCompatActivity {
     SimpleDraweeView draweeView;
+    private PhotoDraweeView mPhotoDraweeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mi_fresco);
         Fresco.initialize(this);
-        draweeView = findViewById(R.id.my_image_view);
-        draweeView.setImageURI("https://cdn.redcanina.es/wp-content/uploads/2013/07/como-cuidar-un-perro-chihuahua.jpg");
+        setContentView(R.layout.activity_mi_fresco);
+        final SimpleDraweeView draweeView = findViewById(R.id.photo_drawee_view);
+        draweeView.setImageURI(Uri.parse("res:///" + R.drawable.panda));
+
 
     }
 }
